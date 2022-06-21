dc510d907d9437ba86dab94091c493ab05ef2061
MohanRaj <mraj86698@gmail.com>
2022-06-21 17:18:26 +0530

    Employee Present or Absent

--- src/empWage/EmpWage.java
+++ src/empWage/EmpWage.java
@@ -5,7 +5,12 @@ public class EmpWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage");
-
+		int employeePresent = 1;
+		int isPresent = (int) (Math.floor(Math.random() * 10) % 2);
+		if(isPresent == employeePresent)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is not Present");
 	}
 
 }
